1e7c70a38183a70dd1a70daa7b92af20632103eb
Ravi <ravi.kiran8844@gmail.com>
2023-06-27 11:40:52 +0530

    added maps section

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import LogoWithTextSection from '@/components/LogoWithTextSection/LogoWithTextSection'
 import VideoSection from '@/components/VideoSection/VideoSection'
+import LocateUsSection from '@/components/LocateUsSection/LocateUsSection'
 
 export default function Home() {
   return (
    <div>
     <LogoWithTextSection/>
     <VideoSection/>
+    <LocateUsSection/>
    </div>
   )
 }
---
+++ components/LocateUsSection/LocateUsSection.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './LocateUsSection.css'
+
+const LocateUsSection = () => {
+  return (
+    <div className='lus_wrapper py-5'>
+      <div className='container p-0'>
+        <div className='col-12 text-center'>
+            <div className='main-title mb-4'>Locate Us</div>
+        </div>
+        <div className='row g-0'>
+            <div className='col-12'>
+                <div className='google-maps'>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.288030512359!2d76.96141777432874!3d11.01700278914676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba858ff7c0bb403%3A0x54541764995a4ffe!2sJewelOne%20Coimbatore!5e0!3m2!1sen!2sin!4v1687844163036!5m2!1sen!2sin" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LocateUsSection
